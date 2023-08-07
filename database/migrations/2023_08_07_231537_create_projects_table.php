64277eedb0fb1f21b3a1cba95b22db164bbb71ff
Anieb22 <anielloverdino@gmail.com>
2023-08-08 01:20:10 +0200

    creato model project e creato colonne DB

---
+++ database/migrations/2023_08_07_231537_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('nome_progetto');
+            $table->text('descrizione');
+            $table->text('passaggi');
+            $table->date('data_di_creazione');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
