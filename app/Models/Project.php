2359405f9e043d4f0e5c33fe592dfe7721be9a6d
Anieb22 <anielloverdino@gmail.com>
2023-08-08 07:54:52 +0200

    creato file index.blade per i progetti

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    protected $fillable = ['azienda', 'nome_progetto', 'descrizione', 'passaggi', 'data_di_creazione'];
 }
--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,8 @@ use App\Http\Controllers\Admin\DashboardController as DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.');
+Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
