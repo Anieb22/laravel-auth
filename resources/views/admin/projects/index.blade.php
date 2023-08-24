aec3b922d57e22c9627f160ddfd94927a2260631
Anieb22 <anielloverdino@gmail.com>
2023-08-24 09:52:34 +0200

    definito destroy (da aggiungere messaggi)

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -30,8 +30,12 @@
                                 <i class="fas fa-eye"></i>
                             </a>
                         </button>
-                        <button type="button" class="btn btn-danger"><a href="#" class="link-underline link-underline-opacity-0 link-light"><i class="fas fa-trash-can"></i></a></button>
-                      <button type="button" class="btn btn-warning"><a href="{{ route('admin.projects.edit', ['project' => $project]) }}" class="link-underline link-underline-opacity-0 link-dark"><i class="fas fa-pen"></i></a></button>
+                        <form action="{{ route('admin.projects.destroy', ['project' => $project]) }}" method="post" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                          <button type="submit" class="btn btn-danger"><i class="fas fa-trash-can"></i></button>
+                        </form>
+                        <button type="button" class="btn btn-warning"><a href="{{ route('admin.projects.edit', ['project' => $project]) }}" class="link-underline link-underline-opacity-0 link-dark"><i class="fas fa-pen"></i></a></button>
                       </td>
                     </tr>
                     @endforeach
